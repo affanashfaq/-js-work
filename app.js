4e9ebe08a6bf06065b8b41ef3548823c45bef3fd
unknown <affanashfaq786@gmail.com>
2022-12-15 15:44:01 +0500

    work

---
+++ app.js
@@ -0,0 +1,42 @@
+var student1 = {
+    name : 'Nasir',
+    rollnum : 174,
+    class : 10,
+    age : 16,
+}
+var student2 = {
+    name : 'Sheriyar',
+    rollnum : 175,
+    class : 10,
+    age : 16,
+}
+var student3 = {
+    name : 'Shahzaib',
+    rollnum : 176,
+    class : 10,
+    age : 16,
+}
+var student4 = {
+    name : 'Affan',
+    rollnum : 177,
+    class : 10,
+    age : 16,
+}
+var student5 = {
+    name : 'Hunain',
+    rollnum : 178,
+    class : 10,
+    age : 16,
+}   
+var a = [student1,student2,student3,student4,student5];
+var inp = document.getElementById("inp");
+var i1 = document.getElementById("i1");
+var i2 = document.getElementById("i2");
+var i3 = document.getElementById("i3");
+var i4 = document.getElementById("i4");
+var i5 = document.getElementById("i5");
+function addval(){
+    if(inp = a){
+        console.log(document.write(a));
+    }
+}
